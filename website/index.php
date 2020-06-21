1d18b244a1edf6201a2efb8d1a3d4a6794576673
Andrew Bennet <me@andrewbennet.com>
2020-06-21 21:58:26 +0100

    Improve website

--- website/index.php
+++ website/index.php
@@ -5,19 +5,32 @@
 
   <body>
     <h1>Timelapse Cam</h1>
+    <h2>Raspberry Pi Zero</h2>
     <div>
-      <a href="./timelapse_photos">All Photos</a>
       <div>
-<?php
-$files = scandir('timelapse_photos', SCANDIR_SORT_DESCENDING);
-$newest_file = $files[0];
-echo "<img width=\"600\" src=\"./timelapse_photos/" . $newest_file . "\"/>";
-echo "<div>" . $newest_file . "</div>";
+        <?php
+          $files = scandir('timelapse_photos', SCANDIR_SORT_DESCENDING);
+          $newest_file = $files[0];
+          echo "<div>Latest Photo: " . $newest_file . "</div>";
+          echo "<img width=\"600\" src=\"./timelapse_photos/" . $newest_file . "\"/>";
 
-$videos = scandir('timelapse_videos', SCANDIR_SORT_DESCENDING);
-$newest_video = $videos[0];
-echo "<video src=\"./timelapse_videos/" . $newest_video . "\"/>";
-?>
+          $videos = scandir('timelapse_videos', SCANDIR_SORT_DESCENDING);
+          $newest_video = $videos[0];
+          echo "<a href=\"./timelapse_videos/" . $newest_video . "\"/>Download Video</a>";
+          echo "<a href=\"./timelapse_photos\">See All Photos</a>";
+        ?>
+      </div>
+      <div>
+        <h3>
+          Raspberry Pi Stats:
+        </h3>
+        <span>
+          Disk space remaining: 
+          <?php
+            echo floor(disk_free_space("/") / (1024 * 1024));
+          ?>
+          MB
+        </span>
       </div>
 
   </body>
